e4f9bfa59ab7259b9e94c66241c48b95ac42a011
Davin Wang <wangd@USEast1.local.alipay.net>
2017-09-11 18:09:43 +0800

    fix sgf bugs

--- sgfutil.py
+++ sgfutil.py
@@ -4,9 +4,19 @@ from datetime import datetime
 
 BOARD_POSITION = 'abcdefghijklmnopqrs'
 
-def WriteBackSGF(game_state, history, filename):
+def GetWinner(game_state):
+    winner = game_state.get_winner()
+    if winner == BLACK:
+        return 'B+'
+    elif winner == WHITE:
+        return 'W+'
+    else:
+        return 'T'
+
+def WriteBackSGF(winner, history, filename, PB=None, PW=None, Komi='7.5'):
     parser = sgf.Parser()
     collection = sgf.Collection(parser)
+    # game properties
     parser.start_gametree()
     parser.start_node()
     parser.start_property('FF') # SGF format version
@@ -15,44 +25,31 @@ def WriteBackSGF(game_state, history, filename):
     parser.start_property('SZ') # Board Size = 19
     parser.add_prop_value('19')
     parser.end_property()
-    parser.start_property('KM') # Komi = 7.5
-    parser.add_prop_value('7.5')
+    parser.start_property('KM') # default Komi = 7.5
+    parser.add_prop_value(str(Komi))
     parser.end_property()
     parser.start_property('PB') # Black Player = Supervised Learning / Reinforced Learning
-    parser.add_prop_value('RL-{}')
+    parser.add_prop_value('RL-{}'.format(PB))
     parser.end_property()
     parser.start_property('PW') # White Player = Supervised Learning / Reinforced Learning
-    parser.add_prop_value('SL-{}')
+    parser.add_prop_value('SL-{}'.format(PW))
     parser.end_property()
     parser.start_property('DT') # Game Date
     parser.add_prop_value(datetime.now().strftime("%Y-%m-%d"))
     parser.end_property()
     parser.start_property('RE') # Result = B+, W+, T
-    winner = game_state.get_winner()
-    if winner == BLACK:
-        parser.add_prop_value('B+')
-        winner = 'B+'
-    elif winner == WHITE:
-        parser.add_prop_value('W+')
-        winner = 'W+'
-    else:
-        parser.add_prop_value('T')
-        winner = 'T'
+    parser.add_prop_value(str(winner))
     parser.end_property()
     parser.end_node()
-    
+    # start of game
     for step in history:
         parser.start_node()
-        parser.start_property(step[0]) # or W
-        parser.add_prop_value(BOARD_POSITION[step[1]]+BOARD_POSITION[step[2]])
+        parser.start_property(str(step[0])) # or W
+        parser.add_prop_value(str(BOARD_POSITION[step[1]]+BOARD_POSITION[step[2]]))
         parser.end_property()
         parser.end_node()
-    
+    # end of game
     parser.end_gametree()
-    
     # record the game in SGF
-    with open(os.path.join('{}_{}_{}.sgf'.format(
-							   filename,
-                               winner,
-                               datetime.now().strftime("%Y-%m-%d"))), "w") as f:
+    with open(os.path.join('{}.sgf'.format(filename)), "w") as f:
         collection.output(f)
