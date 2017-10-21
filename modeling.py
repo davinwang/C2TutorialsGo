11cd913b562d65a0533d332d3c23309de3bf56ef
Davin Wang <davin.wang@live.com>
2017-10-21 22:41:45 +0800

    refine ResNet model

--- modeling.py
+++ modeling.py
@@ -38,16 +38,17 @@ def AddConvModel(model, data, conv_level=13, filters=192, dim_in=48):
 
 def AddResNetModel(model, data, num_blocks=19, filters=256, dim_in=17):
     # Layer 1: 17 x 19 x 19 -conv-> 256 x 19 x 19
-    #pad1 = model.PadImage(data, 'pad1', pad_t=2, pad_l=2, pad_b=2, pad_r=2, mode="constant", value=0.)
-    conv1 = brew.conv(model, data, 'conv1', dim_in=dim_in, dim_out=filters, kernel=3)
+    pad1 = model.PadImage(data, 'pad1', pad_t=1, pad_l=1, pad_b=1, pad_r=1, mode="constant", value=0.)
+    conv1 = brew.conv(model, pad1, 'conv1', dim_in=dim_in, dim_out=filters, kernel=3)
     norm1 = model.Normalize(conv1, 'norm1')
     res_in = brew.relu(model, norm1, 'relu1')
-    # Blocks: 256 x 19 x 19 -conv-> -normalize-> -relu-> -conv-> -normalize-> +INPUT -relu->
+    # Blocks: 256 x 19 x 19 -conv-> -normalize-> -relu-> -conv-> -normalize-> +INPUT -relu-> 256 x 19 x 19
     def AddResBlock(model, input, i, filters, scope='res'):
-        #pad2 = model.PadImage(relu1, 'pad2', pad_t=1, pad_l=1, pad_b=1, pad_r=1, mode="constant", value=0.)
-        conv1 = brew.conv(model, input, 'res/{}/conv1'.format(i), dim_in=filters, dim_out=filters, kernel=3)
+        pad1 = model.PadImage(input, 'res/{}/pad1'.format(i), pad_t=1, pad_l=1, pad_b=1, pad_r=1, mode="constant", value=0.)
+        conv1 = brew.conv(model, pad1, 'res/{}/conv1'.format(i), dim_in=filters, dim_out=filters, kernel=3)
         norm1 = model.Normalize(conv1, 'res/{}/norm1'.format(i))
         relu1 = brew.relu(model, norm1, 'res/{}/relu1'.format(i))
+        pad2 = model.PadImage(relu1, 'res/{}/pad2'.format(i), pad_t=1, pad_l=1, pad_b=1, pad_r=1, mode="constant", value=0.)
         conv2 = brew.conv(model, relu1, 'res/{}/conv2'.format(i), dim_in=filters, dim_out=filters, kernel=3)
         norm2 = model.Normalize(conv2, 'res/{}/norm2'.format(i))
         res = model.Add([norm2, input], 'res/{}/res'.format(i))
@@ -56,12 +57,20 @@ def AddResNetModel(model, data, num_blocks=19, filters=256, dim_in=17):
     for i in range(num_blocks):
         res_out = AddResBlock(model, res_in, i, filters)
         res_in = res_out
-    # 2 layers of Policy head
-    
-    # 3 layers of Value head
-    
-    predict = res_out
-    return predict
+    # Policy Head: 256 x 19 x 19 -conv-> 2 x 19 x 19 -normalize-> -relu-> -FC-> 362
+    ph_conv1 = brew.conv(model, res_out, 'phconv1', dim_in=filters, dim_out=2, kernel=1)
+    ph_norm1 = model.Normalize(ph_conv1, 'phnorm1')
+    ph_relu1 = brew.relu(model, ph_norm1, 'phrelu1')
+    policy = brew.fc(model, ph_relu1, 'policy', dim_in=2*19*19, dim_out=362)
+    # Value Head: 256 x 19 x 19 -conv-> 1 x 19 x 19 -> -normalize-> -relu-> -FC-> 256 x 19 x19 -relu-> -FC-> 1(scalar) -tanh->
+    vh_conv1 = brew.conv(model, res_out, 'vhconv1', dim_in=filters, dim_out=1, kernel=1)
+    vh_norm1 = model.Normalize(vh_conv1, 'vhnorm1')
+    vh_relu1 = brew.relu(model, vh_norm1, 'vhrelu1')
+    vh_fc1 = brew.fc(model, vh_relu1, 'vhfc1', dim_in=1*19*19, dim_out=filters*19*19)
+    vh_relu2 = brew.relu(model, vh_fc1, 'vhrelu2')
+    vh_fc2 = brew.fc(model, vh_relu2, 'vhfc2', dim_in=filters*19*19, dim_out=1)
+    value = brew.tanh(model, vh_fc2, 'value')
+    return policy, value
 
 def AddAccuracy(model, predict, label):
     """Adds an accuracy op to the model"""
