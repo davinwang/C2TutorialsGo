a64345e7580d0d1bac41dbab5fb31ed8e21bc8df
Davin Wang <davin.wang@live.com>
2017-10-31 23:48:13 +0800

    Now loss function for AlphaGo Fan is as correct as v0.1.1

--- modeling.py
+++ modeling.py
@@ -12,16 +12,10 @@ def AddInput(model, batch_size, db, db_type):
     # cast to int
     label_int32 = model.Cast(label_uint16, 'label_int32', to=core.DataType.INT32)
     label = model.FlattenToVec(label_int32, 'label')
-    # encode onehot
-    with core.DeviceScope(core.DeviceOption(caffe2_pb2.CPU, 0)):
-        BOARD_SIZE = model.param_init_net.ConstantFill([], 'BOARD_SIZE', shape=[1,], value=361) # constant
-    label_int64 = model.Cast(label, 'label_int64', to=core.DataType.INT64)
-    onehot = model.OneHot([label_int64, BOARD_SIZE], 'onehot') # shape=(mini_batch,361)
     # don't need the gradient for the backward pass
     data = model.StopGradient(data, data)
     label = model.StopGradient(label, label)
-    onehot = model.StopGradient(onehot, onehot)
-    return data, label, onehot
+    return data, label
 
 def AddConvModel(model, data, conv_level=13, filters=192, dim_in=48):
     # Layer 1: 48 x 19 x 19 -pad-> 48 x 23 x 23 -conv-> 192 x 19 x 19
@@ -39,26 +33,43 @@ def AddConvModel(model, data, conv_level=13, filters=192, dim_in=48):
         input = output
     # Layer 13: 192 x 19 x 19 -conv-> 1 x 19 x 19 -softmax-> 361
     conv13 = brew.conv(model, output, 'conv13', dim_in=filters, dim_out=1, kernel=1)
-    softmax = brew.softmax(model, conv13, 'softmax')
-    predict = model.Flatten(softmax, 'predict')
+    predict = model.Flatten(conv13, 'predict')
     return predict
 
-def AddAccuracy(model, predict, label, log=True):
+def AddSoftmax(model, predict):
+    softmax = brew.softmax(model, predict, 'softmax')
+    return softmax
+
+def AddAccuracy(model, softmax, label, log=True):
     """Adds an accuracy op to the model"""
-    accuracy = brew.accuracy(model, [predict, label], "accuracy")
+    accuracy = brew.accuracy(model, [softmax, label], "accuracy")
     if log:
         model.Print('accuracy', [], to_file=1)
     return accuracy
 
-def AddTrainingOperators(model, predict, expect, base_lr=-0.003, log=True):
+def AddOneHot(model, label):
+    """Decode onehot at modelling, not on the fly
+    """
+    with core.DeviceScope(core.DeviceOption(caffe2_pb2.CPU, 0)):
+        BOARD_SIZE = model.param_init_net.ConstantFill([], 'BOARD_SIZE', shape=[1,], value=361) # constant
+    label_int64 = model.Cast(label, 'label_int64', to=core.DataType.INT64)
+    onehot = model.OneHot([label_int64, BOARD_SIZE], 'onehot') # shape=(mini_batch,361)
+    onehot = model.StopGradient(onehot, onehot)
+    return onehot
+
+def AddTrainingOperators(model, predict, label, expect, base_lr=-0.003, log=True):
     """Adds training operators to the model.
         predict: Predicted distribution by Policy Model
         expect: Expected distribution by MCTS, or transformed from Policy Model
         base_lr: Base Learning Rate. Always fixed
     """
-    xent = model.SigmoidCrossEntropyWithLogits([predict, expect], 'xent')
-    #xent = model.SquaredL2Distance([predict, expect], 'xent')
     # compute the expected loss
+    if label:
+        onehot = AddOneHot(model, label)
+        softmax, xent = model.SoftmaxWithLoss([predict, onehot], ['softmax', 'xent'], label_prob=1)
+        AddAccuracy(model, softmax, label, log)
+    else:
+        softmax, xent = model.SoftmaxWithLoss([predict, expect], ['softmax', 'xent'], label_prob=1)
     loss = model.AveragedLoss(xent, "loss")
     # use the average loss we just computed to add gradient operators to the model
     model.AddGradientOperators([loss])
